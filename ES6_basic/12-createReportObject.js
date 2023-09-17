b380fa4f10a3b27460f312350bc2a330aab8ef92
Nicoou <nf9600362@gmail.com>
2023-09-16 23:01:39 -0700

    task 12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
-  const repoObj = {
-    allemployees: employeesList,
+  const reportObject = {
+    allEmployees: employeesList,
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
-  return repoObj;
+  return reportObject;
 }
