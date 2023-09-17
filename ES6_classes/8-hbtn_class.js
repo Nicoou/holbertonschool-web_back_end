1d592710dc84171f64053a822ed63f98638543e4
Nicoou <nf9600362@gmail.com>
2023-09-17 16:45:38 -0700

    task 8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size
+  }
+
+  toStr() {
+    return this._location
+  }
+}
