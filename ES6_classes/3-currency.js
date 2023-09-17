fd251015b1352a0771bd468ecb07b085e07954f4
Nicoou <nf9600362@gmail.com>
2023-09-17 16:15:26 -0700

    task 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -9,21 +9,15 @@ export default class Currency {
   }
 
   set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._code = this.code
+    this._code = value
   }
 
   get name() {
     return this._name
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = this.name
+  set name(New) {
+    this._name = value
   }
 
   displayFullCurrency() {
